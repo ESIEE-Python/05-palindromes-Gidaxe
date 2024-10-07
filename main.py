d8a9717fbd752399aabff687dfde896d84092491
Gidaxe <107138385+Gidaxe@users.noreply.github.com>
2024-10-07 17:56:34 +0000

    code palyndromes

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+'''
+Programme testant si une chaine de caractères est un palyndrome ou pas.
+'''
+
 #### Fonction secondaire
 
 
-def ispalindrome(p):
+def ispalindrome(s):
 
-    # votre code ici
-    
+    # votre code ici``
+
+    '''
+    Retourne si la chaine de caractères est un palyndrome ou pas.
+
+    Args:
+        s: chaine de caractères.
+
+    Returns:
+        True: Booléen, si s es un palyndrome.
+        False: Booléen, si s n'est pas un palyndrome.
+    '''
+    n = len (s)
+    l = ''
+    for i in range(n-1, -1, -1):
+        l += s[i]
+    if l == s:
+        return True
+
     return False
 
 #### Fonction principale
@@ -13,6 +34,10 @@ def ispalindrome(p):
 def main():
 
     # vos appels à la fonction secondaire ici
+
+    '''
+    Fonction principale
+    '''
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
